3be1129f7972844c7e5e661d6aa644907964ba59
gmacharia <42572358+gmacharia@users.noreply.github.com>
2021-09-30 02:01:37 +0300

    Update deployment.sh
    Updated Deployment sh with correct values

--- deployment.sh
+++ deployment.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+echo " ********** BEGIN ********** "
+
+application=kyosk-config-service
+
+docker_username=$1
+docker_password=$2
+docker_image=$3
 
 ## Deploy to Minikube.
 ## First We Create A Namespace.
+
 kubectl create namespace kyosk
 
 ## Sleep for a few to make sure the pod is deployed
 sleep 5m
 
 ## Now We deploy our service.
-kubectl -n kyosk create deployment $applicationName --image=$dockerUsername/$applicationName:v$applicationVersion-$SUBSTRING-$applicationArchitectureTag
+kubectl -n kyosk create deployment $application --image=$docker_username/$docker_image
 
 ## Sleep for 5m to wait for pod to start running.
 sleep 5m
@@ -16,4 +24,4 @@ sleep 5m
 ## Expose the service using via loadBalancer IP.
 ## On Other clusters, the IP is generated as an external IP.
 ## On Minikube, the External IP will be marked as "Pending" and you should run "minikube service -n staging  kyosk-config-service" to expose the service externally.
-kubectl -n kyosk expose deployment $applicationName --type=LoadBalancer --port=80 --target-port=8080
+kubectl -n kyosk expose deployment $application --type=LoadBalancer --port=80 --target-port=8080
